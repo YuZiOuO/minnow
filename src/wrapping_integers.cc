0991e13850d83b0e38c8bf2389e7f41aa10a6584
kanata <cyz050312@gmail.com>
2025-08-01 18:27:01 +0800

    check2: naive impl of Wrap32

--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -5,14 +5,25 @@ using namespace std;
 
 Wrap32 Wrap32::wrap( uint64_t n, Wrap32 zero_point )
 {
-  // Your code here.
-  debug( "unimplemented wrap( {}, {} ) called", n, zero_point.raw_value_ );
-  return Wrap32 { 0 };
+  return Wrap32 { static_cast<uint32_t>(n) + zero_point.raw_value_ };
 }
 
 uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
 {
-  // Your code here.
-  debug( "unimplemented unwrap( {}, {} ) called", zero_point.raw_value_, checkpoint );
-  return {};
+  uint64_t prefix = checkpoint - static_cast<uint32_t>(checkpoint);
+  uint32_t zero_offset = this->raw_value_ - zero_point.raw_value_;
+  constexpr uint64_t power32 = (1UL << 32);
+  uint64_t seqno_major = prefix + zero_offset;
+  uint64_t seqno_minor = seqno_major > checkpoint ? seqno_major - power32:seqno_major + power32;
+  if(seqno_major == checkpoint || (seqno_major < power32 && seqno_major > checkpoint)){
+    return seqno_major;
+  }
+
+  // FIXME: it happends that Wrap(1<<63-1).unwrap(0,1<<63-2) was correctly handled,
+  // but not considered.
+  if(seqno_major > checkpoint){
+    return seqno_major - checkpoint > checkpoint - seqno_minor ? seqno_minor : seqno_major;
+  }else{
+    return checkpoint - seqno_major > seqno_minor - checkpoint ? seqno_minor : seqno_major;
+  }
 }
