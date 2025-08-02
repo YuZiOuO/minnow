cb75a918602afda4adc148850865f0951f427092
kanata <cyz050312@gmail.com>
2025-08-02 18:14:30 +0800

    check2: tcp_receiver impl

--- src/tcp_receiver.cc
+++ src/tcp_receiver.cc
@@ -1,18 +1,33 @@
 #include "tcp_receiver.hh"
 #include "debug.hh"
+#include "wrapping_integers.hh"
 
 using namespace std;
 
 void TCPReceiver::receive( TCPSenderMessage message )
 {
-  // Your code here.
-  debug( "unimplemented receive() called" );
-  (void)message;
+  if ( message.RST ) {
+    reader().set_error();
+  }
+  if ( message.SYN && !ISN.has_value() ) {
+    ISN.emplace( message.seqno );
+  }
+  if ( ISN.has_value() ) {
+    auto abs_seqno = message.seqno.unwrap( ISN.value(), writer().bytes_pushed() );
+    if ( !( abs_seqno == 0 && !message.SYN && !message.payload.empty() ) ) {
+      // special case: trying to override seqno occupied by SYN.
+      uint64_t stream_index = abs_seqno ? abs_seqno - 1 : 0;
+      reassembler_.insert( stream_index, message.payload, message.FIN );
+    }
+  }
 }
 
 TCPReceiverMessage TCPReceiver::send() const
 {
-  // Your code here.
-  debug( "unimplemented send() called" );
-  return {};
+  auto ack_seqno = ISN.has_value()
+                     ? Wrap32::wrap( ISN.has_value() + writer().bytes_pushed() + writer().is_closed(), ISN.value() )
+                     : std::optional<Wrap32>();
+  uint64_t available_capacity
+    = min( writer().available_capacity(), static_cast<uint64_t>( std::numeric_limits<uint16_t>::max() ) );
+  return { ack_seqno, static_cast<uint16_t>( available_capacity ), reader().has_error() };
 }
--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -5,26 +5,27 @@ using namespace std;
 
 Wrap32 Wrap32::wrap( uint64_t n, Wrap32 zero_point )
 {
-  return Wrap32 { static_cast<uint32_t>(n) + zero_point.raw_value_ };
+  return Wrap32 { static_cast<uint32_t>( n ) + zero_point.raw_value_ };
 }
 
 uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
 {
-  constexpr uint64_t UINT32_RANGE = (1UL << 32);
-  
+  constexpr uint64_t UINT32_RANGE = ( 1UL << 32 );
+
   // seqno_major and minor represents the direct neighbor of the checkpoint.
-  uint64_t prefix = checkpoint - static_cast<uint32_t>(checkpoint);
+  uint64_t prefix = checkpoint - static_cast<uint32_t>( checkpoint );
   uint32_t zero_offset = this->raw_value_ - zero_point.raw_value_;
   uint64_t seqno_major = prefix + zero_offset;
   uint64_t seqno_minor = seqno_major > checkpoint ? seqno_major - UINT32_RANGE : seqno_major + UINT32_RANGE;
 
   // compare which one is nearer
-  int64_t major_s = static_cast<int64_t>(seqno_major);
-  int64_t minor_s = static_cast<int64_t>(seqno_minor);
-  int64_t checkpoint_s = static_cast<int64_t>(checkpoint);
-  //special case: different sign, marking the minor has overflowed.
-  if((major_s ^ minor_s) < 0){ 
-    return seqno_major; 
+  int64_t major_s = static_cast<int64_t>( seqno_major );
+  int64_t minor_s = static_cast<int64_t>( seqno_minor );
+  int64_t checkpoint_s = static_cast<int64_t>( checkpoint );
+
+  // special case: different sign, marking the minor has overflowed.
+  if ( ( major_s ^ minor_s ) < 0 ) {
+    return seqno_major;
   }
-  return abs(major_s - checkpoint_s) < abs(minor_s - checkpoint_s) ? seqno_major:seqno_minor;
+  return abs( major_s - checkpoint_s ) < abs( minor_s - checkpoint_s ) ? seqno_major : seqno_minor;
 }
